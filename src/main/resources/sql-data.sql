0efdb66f6bf0cf33156e1a543f7fe6fad44e3e22
Sue Bluck <SBluck@academytrainee.com>
2020-11-27 17:20:53 +0000

    SB: update sql schema and data to include item, order and order_item tables

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,12 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jane', 'green');
+INSERT INTO `ims`.`items` (`name`, `value`) VALUES ('hand sanitiser 50ml', 4.25);
+INSERT INTO `ims`.`items` (`name`, `value`) VALUES ('reusable mask medium', 6.50);
+INSERT INTO `ims`.`items` (`name`, `value`) VALUES ('reusable mask small', 5.40);
+INSERT INTO `ims`.`orders` (`customer_id`) VALUES (2);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`) VALUES (1, 1);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`) VALUES (1, 2);
+INSERT INTO `ims`.`orders` (`customer_id`) VALUES (1);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`) VALUES (2, 2);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`) VALUES (2, 3);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`) VALUES (2, 1);
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -7,3 +7,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NULL DEFAULT NULL,
+    `value` DECIMAL(5,2) NULL DEFAULT NULL,
+    `quantity` INT(4) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+     PRIMARY KEY (`id`),
+     CONSTRAINT `fk_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+    `order_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`),
+    CONSTRAINT `fk_item_id` FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
+);
