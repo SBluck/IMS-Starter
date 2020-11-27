2bbe3c4bb27fc8565e6e2d7a5ab555b6df3dd387
Sue Bluck <SBluck@academytrainee.com>
2020-11-27 17:26:52 +0000

    SB: update test sql data and schema to include item, order and order_item tables

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,4 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`items` (`name`, `value`) VALUES ('hand sanitiser 50ml', 4.25);
+INSERT INTO `ims`.`orders` (`customer_id`) VALUES (1);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`) VALUES (1, 1);
